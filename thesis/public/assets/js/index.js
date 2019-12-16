895548a1103674938299856eebf3a8158b451779
aldowjy <aldowjy12@gmail.com>
2019-12-16 22:07:08 +0700

    Update form user registration, and create student dashboard screen

--- thesis/public/assets/js/index.js
+++ thesis/public/assets/js/index.js
@@ -10,17 +10,12 @@ $(document).ready(function() {
         if($('.type').val() == 'student') {
             $('.entryStudent').show();
             $('.entryLecturer').hide();
-            $('.entryLecturer form').get(0).reset();
         } else if ($('.type').val() == 'lecturer') {
             $('.entryLecturer').show();
             $('.entryStudent').hide();
-            $('.entryStudent form').get(0).reset();
         } else {
             $('.entryLecturer').hide();
             $('.entryStudent').hide();
-            $('.entry input').val('');
-            $('.entryLecturer form').get(0).reset();
-            $('.entryStudent form').get(0).reset();
         }
     });
 });
