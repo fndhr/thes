9da2ffb43d47a69ac21b3651d7b025a9141848f0
aldowjy <aldowjy12@gmail.com>
2019-12-18 11:14:35 +0700

    Fixing style admin screen

--- thesis/resources/views/admin/studentdetail.blade.php
+++ thesis/resources/views/admin/studentdetail.blade.php
@@ -19,9 +19,54 @@
                 <div class="col-1">&#10003;</div>
             </div>
             <div class="row py-2 mb-2">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-hover">
+                        <thead class="thead-dark text-center">
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Restourant Management System for Bubble Tea Stall</td>
+                                <td><span class="text-success">YES</span>&emsp;<span class="text-danger">NO</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row py-2 mb-2">
                 <div class="col-3">Advisor</div>
                 <div class="col-7">:&nbsp;&nbsp;Rikip Ginanjar, M.Sc</div>
                 <div class="col-1">&#10003;</div>
+            </div>
+            <div class="row py-2 mb-2">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-hover">
+                        <thead class="thead-dark text-center">
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Advisor</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Rila Mandala Ph.D</td>
+                                <td><span class="text-success">YES</span>&emsp;<span class="text-danger">NO</span></td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>Rikip Ginanjar M.Sc</td>
+                                <td><span class="text-success">YES</span>&emsp;<span class="text-danger">NO</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <div class="row py-2 mb-2">
                 <div class="col-3">Proposal</div>
--- thesis/resources/views/admin/studentregistration.blade.php
+++ thesis/resources/views/admin/studentregistration.blade.php
@@ -9,11 +9,11 @@
     </div>
     <div class="row justify-content-center">
         <div class="col-11">
-            <form method="POST" action="/register/lecturer">
+            <form  method="POST" action="/register/student">
                 @csrf
                 <div class="form-group row">
                     <label class="col-3 col-form-label">First Name</label>
-                    <input type="text" class="form-control col-9" for="first_name" placeholder="example">
+                    <input type="text" class="form-control col-9 @error('user_name') is-invalid @enderror" for="first_name" name="first_name" placeholder="example" value="{{old('first_name')}}">
                     @error('first_name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -22,7 +22,7 @@
                 </div>
                 <div class="form-group row">
                     <label class="col-3 col-form-label">Last Name</label>
-                    <input type="text" class="form-control col-9" for="last_name" placeholder="example">
+                    <input type="text" class="form-control col-9" for="last_name" name="last_name" placeholder="example">
                     @error('last_name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -31,42 +31,42 @@
                 </div>
                 <div class="form-group row">
                     <label class="col-3 col-form-label">Username</label>
-                    <input type="text" class="form-control col-9" for="user_name" placeholder="example">
-                    @error('first_name')
+                    <input type="text" class="form-control col-9 @error('username') is-invalid @enderror" for="username" name="username" placeholder="example" value="{{old('username')}}">
+                    @error('username')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
                 <div class="form-group row">
-                    <label class="col-3 col-form-label">Lecturer ID</label>
-                    <input type="text" class="form-control col-9" for="lec_id" placeholder="123456789">
-                    @error('first_name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="form-group row">
-                    <label class="col-3 col-form-label">Role</label>
-                    <div class="col-9 pt-2 pl-0" id="roleLecturer">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" for="isAdv" name="isAdv" id="advisor" value="option1">
-                            <label class="form-check-label" for="advisor">Advisor</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" for="isExm" name="isExm" id="examiner" value="option2">
-                            <label class="form-check-label" for="examiner">Examiner</label>
-                        </div>
-                    </div>
+                    <label class="col-3 col-form-label">Student ID</label>
+                    <input type="text" class="form-control col-9" for="std_id" name="std_id" placeholder="123456789">
                 </div>
                 <div class="form-group row">
                     <label class="col-3 col-form-label">Phone Number</label>
-                    <input type="text" class="form-control col-9" for="phone" placeholder="081234567890">
+                    <input type="text" class="form-control col-9" for="phone" name="phone" placeholder="081234567890">
                 </div>
                 <div class="form-group row">
                     <label class="col-3 col-form-label">Email Address</label>
-                    <input type="email" class="form-control col-9" for="email" placeholder="name@example.com">
+                    <input type="email" class="form-control col-9" for="email" name="email" placeholder="name@example.com">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group row">
+                    <label class="col-3 col-form-label">Password</label>
+                    <input type="password" class="form-control col-9" for="password" name="password" placeholder="name@example.com">
+                </div>
+                <div class="form-group row">
+                    <label class="col-3 col-form-label">Major</label>
+                    <select class="form-control col-3" for="major_id" name="major_id">
+                        <option selected>Choose...</option>
+                        <option value="1">Information System</option>
+                        <option value="2">Information Technology</option>
+                        <option value="3">Visual Design Graphic</option>
+                    </select>
                 </div>
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary px-5 my-4 btnSubmit">Save</button>
