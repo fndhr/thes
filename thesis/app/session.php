4d0430453f14b1ab4c9fa902e0fb240553e93ea4
minatorisato <36133909+minatorisato@users.noreply.github.com>
2019-12-25 23:42:01 +0700

    perbaikan blocker aldo

--- thesis/app/session.php
+++ thesis/app/session.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class session extends Model
 {
     //
+    public $primaryKey = 'session_id';
     public function student(){
         return $this->belongsTo('App\student','session_id','session_id');
     }
