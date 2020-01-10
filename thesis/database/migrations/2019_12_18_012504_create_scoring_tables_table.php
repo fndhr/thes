eee70d7c747e9d9f65a300d98ec90191a93fd143
tdykr <30433965+tdykr@users.noreply.github.com>
2020-01-10 23:03:27 +0700

    update db scoringler

--- thesis/database/migrations/2019_12_18_012504_create_scoring_tables_table.php
+++ thesis/database/migrations/2019_12_18_012504_create_scoring_tables_table.php
@@ -20,6 +20,8 @@ class CreateScoringTablesTable extends Migration
             $table->integer('supervisor_total')->default(0);
             $table->string('std_id');
             $table->string('lec_id');
+            $table->string('suggestion');
+            $table->string('correction');
             $table->foreign('std_id')->references('std_id')->on('students');
             $table->foreign('lec_id')->references('lec_id')->on('lecturers');
             $table->timestamps();
