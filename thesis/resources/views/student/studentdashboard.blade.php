6b8bd7624b7a730792dee261dee77f8bb32efed3
tdykr <30433965+tdykr@users.noreply.github.com>
2020-01-15 00:43:58 +0700

    refix card

--- thesis/resources/views/student/studentdashboard.blade.php
+++ thesis/resources/views/student/studentdashboard.blade.php
@@ -518,15 +518,9 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <input type="file" class="col-3 form-control-file" id="file" for="file" name="file">
                                 </div>
-                                @if($student->session->passed_revised_doc_dt || count($student->documentUpload)>=4)
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-secondary btn-pill px-5 my-3 btnSubmitRevisedDoc" disabled>Submit</button>
-                                    </div>
-                                @else
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-success btn-pill px-5 my-3 btnSubmitRevisedDoc">Submit</button>
-                                    </div>
-                                @endif                                
+                                <div class="text-center">
+                                   <button type="submit" class="btn btn-success btn-pill px-5 my-3 btnSubmitRevisedDoc" >Submit</button>
+                                </div>                          
                             </form>
                         </div>
                     </div>
@@ -589,15 +583,9 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <input type="file" class="col-3 form-control-file" id="file" for="file" name="file">
                                 </div>
-                                @if($student->session->passed_finalized_doc_dt || count($student->documentUpload)>=5)
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-secondary btn-pill px-5 my-3 btnSubmitFinalized" disabled>Submit</button>
-                                    </div>
-                                @else
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-success btn-pill px-5 my-3 btnSubmitFinalized">Submit</button>
-                                    </div>
-                                @endif                                
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-success btn-pill px-5 my-3 btnSubmitFinalized">Submit</button>
+                                </div>                            
                             </form>
                         </div>
                     </div>
