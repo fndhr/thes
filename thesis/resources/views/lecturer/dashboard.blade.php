6ac42fa8b740c020f4ea1a1a659fae145b19d1bc
aldowjy <aldowjy12@gmail.com>
2020-01-20 13:45:44 +0700

    add chart template on admin and lecturer dashboard

--- thesis/resources/views/lecturer/dashboard.blade.php
+++ thesis/resources/views/lecturer/dashboard.blade.php
@@ -8,7 +8,7 @@
         </div>
     </div>
     <div class="row my-5">
-        <div class="col-6">
+        <div class="col-3">
             <a href="/lecturer/studentSearch">
                 <div class="text-center">
                     <img src="/assets/image/admin_studentsearch.png" width="100" height="100" class="d-inline-block align-top my-4" alt="">
@@ -16,7 +16,7 @@
                 </div>
             </a>
         </div>
-        <div class="col-6">
+        <div class="col-3">
             <a href="/lecturer/defenseScheduleSearch">
                 <div class="text-center mx-3">
                     <img src="/assets/image/admin_searchschedule.png" width="100" height="100" class="d-inline-block align-top my-4" alt="">
@@ -24,6 +24,34 @@
                 </div>
             </a>
         </div>
+        <div class="col-6">
+            <canvas id="sessionChart"></canvas>
+        </div>
     </div>
 </div>
+
+<script src="/assets/js/chartjs.min.js"></script>
+<script>
+    var ctx = document.getElementById('sessionChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Client 1', 'Client 2', 'Client 3'],
+            datasets: [{
+                label: 'isApproved',
+                data: [12, 19, 3],
+                backgroundColor: 'rgba(54, 162, 235, 0.2)'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+</script>
 @endsection
